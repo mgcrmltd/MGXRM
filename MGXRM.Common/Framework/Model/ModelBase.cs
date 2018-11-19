99e26879e85dae194c7a25b160b0f47742427b21
mogvinda <mogvinda@hotmail.com>
2018-11-19 18:59:24 +0000

    Added model base

---
+++ MGXRM.Common/Framework/Model/ModelBase.cs
@@ -0,0 +1,19 @@
+﻿using MGXRM.Common.Framework.Interfaces;
+using Microsoft.Xrm.Sdk;
+
+namespace MGXRM.Common.Framework.Model
+{
+    public abstract class ModelBase<T> where T : Entity
+    {
+        protected IImageManager<T> Images { get; }
+        protected IContextManager Context { get; }
+        protected IRepository Repository { get; }
+
+        protected ModelBase(IImageManager<T> images, IContextManager context, IRepository repository)
+        {
+            Images = images;
+            Context = context;
+            Repository = repository;
+        }
+    }
+}
---
+++ MGXRM.Common/MGXRM.Common.Tests/Framework/Model/ModelBaseTest.cs
@@ -0,0 +1,52 @@
+﻿using FakeItEasy;
+using MGXRM.Common.Framework.Interfaces;
+using MGXRM.Common.Framework.Model;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace MGXRM.Common.Tests.Framework.Model
+{
+    public class ModelBaseTest
+    {
+        [Fact]
+        public void Context_Images_Repository_Set_In_Constructor()
+        {
+            var images = A.Fake<IImageManager<TestEntityClass>>();
+            var context = A.Fake<IContextManager>();
+            var repo = A.Fake<IRepository>();
+            var model = new TestModelBaseClass(images,context,repo);
+            Assert.Same(images, model.GetImageManager());
+            Assert.Same(context, model.GetContextManager());
+            Assert.Same(repo, model.GetRepository());
+        }
+    }
+
+    #region Test classes
+    public class TestModelBaseClass : ModelBase<TestEntityClass>
+    {
+        public TestModelBaseClass(IImageManager<TestEntityClass> images, IContextManager context, IRepository repository) :
+            base(images, context, repository)
+        {
+        }
+
+        public IImageManager<TestEntityClass> GetImageManager()
+        {
+            return Images;
+        }
+
+        public IContextManager GetContextManager()
+        {
+            return Context;
+        }
+
+        public IRepository GetRepository()
+        {
+            return Repository;
+        }
+    }
+
+    public class TestEntityClass : Entity
+    {
+    }
+    #endregion
+}
