a394a0b3939fe7aa06ac11404ca6f01e56dfa3a9
mogvinda <mogvinda@hotmail.com>
2018-11-21 16:58:36 +0000

    workflowcontrollerbase initial commit

---
+++ MGXRM.Common/Framework/Controller/WorkflowControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace MGXRM.Common.Framework.Controller
+{
+    public abstract class WorkflowControllerBase<T> where T : Entity
+    {
+        protected IWorkflowContext Context { get; }
+
+        protected WorkflowControllerBase(IWorkflowContext context)
+        {
+            Context = context;
+        }
+    }
+}
---
+++ MGXRM.Common/MGXRM.Common.Tests/Framework/Controller/WorkflowControllerBaseTest.cs
@@ -0,0 +1,27 @@
+﻿using MGXRM.Common.Framework.Controller;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+using FakeItEasy;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace MGXRM.Common.Tests.Framework.Controller
+{
+    public class WorkflowControllerBaseTest
+    {
+        [Fact]
+        public void Context_Set_In_Constructor()
+        {
+            var context = A.Fake<IWorkflowContext>();
+            var controller = new TestWorkflowControllerBaseClass(context);
+            Assert.Same(context, controller.BaseExecutionContext);
+        }
+    }
+
+    public class TestWorkflowControllerBaseClass : WorkflowControllerBase<Entity>
+    {
+        public IWorkflowContext BaseExecutionContext => base.Context;
+        public TestWorkflowControllerBaseClass(IWorkflowContext context) : base(context)
+        {
+        }
+    }
+}
